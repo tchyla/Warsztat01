84a74aa1220a727aa2d0b2a5dcb472e10e9fd469
tchyla <chylatomek@gmail.com>
2021-10-09 17:57:31 +0200

    Start

--- src/main/java/pl/codersLabs/MainTest.java
+++ src/main/java/pl/codersLabs/MainTest.java
@@ -5,11 +5,5 @@ import org.apache.commons.lang3.StringUtils;
 public class MainTest {
     
     public static void main(String[] args) {
-        String s = StringUtils.deleteWhitespace(" dsdv ds   d");
-        System.out.println(s);
-
-        System.out.println(ConsoleColors.RED + " red ");
-
-        System.out.println(ConsoleColors.BLUE + " blue ");
     }
 }
